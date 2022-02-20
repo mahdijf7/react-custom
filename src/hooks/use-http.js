f75d9fc82a6bb079627bacac45b3bf64764af7b8
mahdijf7 <mahdi.1998j@gmail.com>
2022-02-20 21:40:06 +0330

    initial-commit

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,6 +1,6 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback } from 'react';
 
-const useFetch = () => {
+const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -9,19 +9,19 @@ const useFetch = () => {
     setError(null);
     try {
       const response = await fetch(requestConfig.url, {
-        method: requestConfig.method ? requestConfig.method : "GET",
+        method: requestConfig.method ? requestConfig.method : 'GET',
         headers: requestConfig.headers ? requestConfig.headers : {},
         body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
 
       if (!response.ok) {
-        throw new Error("Request failed!");
+        throw new Error('Request failed!');
       }
 
       const data = await response.json();
       applyData(data);
     } catch (err) {
-      setError(err.message || "Something went wrong!");
+      setError(err.message || 'Something went wrong!');
     }
     setIsLoading(false);
   }, []);
@@ -33,4 +33,4 @@ const useFetch = () => {
   };
 };
 
-export default useFetch;
+export default useHttp;
